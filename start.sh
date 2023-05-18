83378e53a5897006162f3ccf5b907f341e5a9383
LOOK ME FROM EARTH <102791126+DHANANJAY4532@users.noreply.github.com>
2023-05-18 19:05:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/DHANANJAY4532/V5-Over-POWER.git /V5-Over-POWER
+  git clone https://github.com/DHANANJAY4532/V5-Over-POWER.git /V5OverPOWER
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /V5-Over-POWER
+  git clone $UPSTREAM_REPO /V5OverPOWER
 fi
-cd /v4_overpower
+cd /V5OverPOWER
 pip3 install -U -r requirements.txt
 echo "Starting...."
 python3 bot.py
