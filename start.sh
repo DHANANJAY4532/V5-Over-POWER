c47edd3c79bc4c8ca59b691e86f5dec4283d9c88
LOOK ME FROM EARTH <102791126+DHANANJAY4532@users.noreply.github.com>
2023-05-18 15:58:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/DHANANJAY4532/v4_overpower.git /v4_overpower
+  git clone https://github.com/DHANANJAY4532/V5-Over-POWER.git /V5-Over-POWER
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /v4_overpower
+  git clone $UPSTREAM_REPO /V5-Over-POWER
 fi
 cd /v4_overpower
 pip3 install -U -r requirements.txt
